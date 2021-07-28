149689c0869d9a73f3a6620954f7d1302d0fc598
danielbossier <dbossier21@gmail.com>
2021-07-28 13:23:31 -0700

    added signup, login, logout pages

---
+++ src/views/TeamsIndex.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="teams-index">
+    <div v-for="team in teams" :key="team.id">
+      <router-link v-bind:to="`/teams/${team.id}`">
+        <h2>Team: {{ team.name }}</h2>
+        <p>Wins: {{ team.wins }}</p>
+        <p>Losses: {{ team.losses }}</p>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      teams: [],
+      currentTeam: {},
+    };
+  },
+  created: function () {
+    this.indexTeams();
+  },
+  methods: {
+    indexTeams: function () {
+      axios.get("/teams").then((response) => {
+        this.teams = response.data;
+        console.log("All teams:", this.teams);
+      });
+    },
+  },
+};
+</script>
