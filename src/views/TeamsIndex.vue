1d058f793ca419a97b8677f581e6c20bf5a4b1c0
danielbossier <dbossier21@gmail.com>
2021-08-19 12:04:10 -0700

    reset index and mlb standings

--- src/views/TeamsIndex.vue
+++ src/views/TeamsIndex.vue
@@ -2,11 +2,16 @@
   <div class="teams-index">
     <div v-for="team in teams" :key="team.id">
       <router-link v-bind:to="`/teams/${team.id}`">
-        <h2>{{ team.name }}</h2>
-        <p>Current Record:</p>
-        <p>{{ team.wins }} - {{ team.losses }}</p>
+        <h2>{{ team.city }} {{ team.name }}</h2>
+        <h3>{{ team.wins }} - {{ team.losses }}</h3>
       </router-link>
     </div>
+    <!-- <div v-for="team in teams[1]" :key="team.id">
+      <router-link v-bind:to="`/teams/${team.id}`">
+        <h2>{{ team.name }}</h2>
+        <h3>{{ team.wins }} - {{ team.losses }}</h3>
+      </router-link>
+    </div> -->
   </div>
 </template>
 
