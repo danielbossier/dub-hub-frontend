e914264f070626497d48348c4fc75a7837e14d0a
danielbossier <dbossier21@gmail.com>
2021-08-20 12:32:29 -0700

    added heading

--- src/App.vue
+++ src/App.vue
@@ -162,9 +162,9 @@
       <div id="layoutSidenav_content">
         <main>
           <div class="container-fluid px-4">
-            <h1 class="mt-4">Dashboard</h1>
+            <h1 class="mt-4">Welcome to The Dub Hub!</h1>
             <ol class="breadcrumb mb-4">
-              <li class="breadcrumb-item active">Dashboard</li>
+              <li class="breadcrumb-item active">Keeping Track of them Dubs</li>
             </ol>
           </div>
         </main>
